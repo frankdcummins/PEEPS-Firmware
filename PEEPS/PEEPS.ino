b1f45299c029d925c9055963635dd9272b408c6b
Frank <fdcummins@gmail.com>
2021-04-12 19:46:06 -0500

    Updated firmware to handle pump time limits
    -Frank

--- PEEPS/PEEPS.ino
+++ PEEPS/PEEPS.ino
@@ -15,20 +15,38 @@
 // ===================================================================================== //
 
 // Define the sample period in milliseconds
-const byte SAMPLE_PERIOD = 10
+const byte SAMPLE_PERIOD = 10;
+const byte MOVING_AVERAGE_WINDOW = 16;
+
+// Pins
+const byte PIN_AIR_PUMP = 10;
+
+// Air Pump
+const byte AIR_PUMP_DUTY_CYCLE = 230;
+const int TIME_PUMP_REST = 5000;
+const int TIME_PUMP_ON = 10000;
 
 // ===================================================================================== //
 // Members
 // ===================================================================================== //
 
-// Sampling values
-unsigned long timeOfLastSample = 0L;
+// Sampling variables
+unsigned long timeOfLastSample = 0UL;
+float movingAverage[MOVING_AVERAGE_WINDOW];
 
-// Analog sensor values
+// Analog sensor variables
 const byte nSensors = 2;
 const int sensorPins[] = {A0, A1};
 const String sensorNames[] = {"0: ", "1: "};
 int sensorReadings[] = {0, 0};
+
+// Air pump variables
+unsigned long timeSinceLastPumpStart = 0;
+unsigned long timeSinceLastPumpStop = 0;
+bool pumpState = false;
+
+// Serial control variables
+String serialMessage = "";
 
 // ===================================================================================== //
 // Setup Method
@@ -37,10 +55,10 @@ int sensorReadings[] = {0, 0};
 void setup()
 {
   // Start the serial bus at 115200 bps
-  Serial.begin(115200);
+  SerialUSB.begin(115200);
 
-  // Wait a bit for things to get started
-  delay(100);
+  pinMode(PIN_AIR_PUMP, OUTPUT);
+  analogReadResolution(12);
 }
 
 // ===================================================================================== //
@@ -48,6 +66,23 @@ void setup()
 // ===================================================================================== //
 
 void loop()
+{
+  // Run Pressure Sensor Feature
+  RunPressureSensorFeature();
+
+  // Run Air Pump Feature
+  RunAirPumpFeature();
+
+  // Method to handle serial events
+  HandleSerialEvents();
+}
+
+// ===================================================================================== //
+// Auxiliary Methods
+// ===================================================================================== //
+
+// Method to run the pressure sensor feature
+void RunPressureSensorFeature()
 {
   // If SAMPLE_PERIOD has elapsed
   if (millis() - timeOfLastSample >= SAMPLE_PERIOD)
@@ -61,11 +96,81 @@ void loop()
     // Update the timer variable
     timeOfLastSample = millis();
   }
+  else
+  {
+    return;
+  }
 }
 
-// ===================================================================================== //
-// Auxiliary Methods
-// ===================================================================================== //
+// ------------------------------------------------------------------------------------- //
+
+// Method to run the air pump feature
+void RunAirPumpFeature()
+{
+  // If the pump is to be turned on
+  if (pumpState == true)
+  {
+    // If more than TIME_PUMP_REST has elapsed since the last recording and
+    // the time since the pump started is less than TIME_PUMP_ON
+    if (millis() - timeSinceLastPumpStop > TIME_PUMP_REST &&
+        millis() - timeSinceLastPumpStart < TIME_PUMP_ON)
+    {
+      // Turn on the pump
+      analogWrite(PIN_AIR_PUMP, AIR_PUMP_DUTY_CYCLE);
+    }
+    else
+    {
+      // Lower the pump state flag
+      pumpState = false;
+
+      // Turn off the pump
+      analogWrite(PIN_AIR_PUMP, 0);
+
+      // Update the timer variable
+      timeSinceLastPumpStop = millis();
+    }
+  }
+  else
+  {
+    return;
+  }
+}
+
+// ------------------------------------------------------------------------------------- //
+
+// Method to parse serial commands
+void parseCommand(String message)
+{
+  // If the serial terminal received an R for record
+  if (serialMessage.equals("R"))
+  {
+    // Raise the pump state flag
+    pumpState = true;
+
+    // Update the timer variable
+    timeSinceLastPumpStart = millis();
+  }
+}
+
+// ------------------------------------------------------------------------------------- //
+
+// Method to calculate moving average
+int calculateMovingAverage(int value)
+{
+  float sumMeasurements = 0;
+
+  for (uint8_t i = MOVING_AVERAGE_WINDOW - 1; i > 0; --i)
+  {
+    movingAverage[i] = movingAverage[i - 1];
+    sumMeasurements += movingAverage[i];
+  }
+  movingAverage[0] = float(value);
+  sumMeasurements += movingAverage[0];
+
+  return (int)(sumMeasurements / MOVING_AVERAGE_WINDOW);
+}
+
+// ------------------------------------------------------------------------------------- //
 
 // Method to sample the sensors
 void sampleSensors()
@@ -75,6 +180,9 @@ void sampleSensors()
   {
     sensorReadings[sensor] = analogRead(sensorPins[sensor]);
   }
+
+  // sensorReadings[0] = calculateMovingAverage(analogRead(sensorPins[1]));
+  // sensorReadings[1] = analogRead(sensorPins[1]);
 }
 
 // ------------------------------------------------------------------------------------- //
@@ -94,9 +202,37 @@ void printReadingsToSerial()
   values.concat(String(sensorReadings[nSensors - 1]));
 
   // Print the values string to the serial port
-  Serial.println(values);
+  SerialUSB.println(values);
 }
 
 // ------------------------------------------------------------------------------------- //
 
+// Method to handle serial events
+void HandleSerialEvents()
+{
+  // If there is any SerialUSB data available, read it
+  if (SerialUSB.available())
+  {
+    // Read a byte from the SerialUSB communication buffer
+    char serialInput = SerialUSB.read();
+
+    // If we have reached the end of the message, begin processing it
+    if (serialInput == '\n')
+    {
+      // Parse the command
+      parseCommand(serialMessage);
+
+      // Clear the SerialUSB message
+      serialMessage = "";
+    }
+
+    // Otherwise, continue building the incoming SerialUSB message
+    else
+    {
+      serialMessage += serialInput;
+    }
+  }
+}
+
+// ------------------------------------------------------------------------------------- //
 // end of file
